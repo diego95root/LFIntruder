4d75e91c07917f59bd7b36eccfabe4672a1e8ff1
DiegoBernal <sc18dba@leeds.ac.uk>
2019-02-27 22:39:17 +0000

    Added no option support, banner

--- main.py
+++ main.py
@@ -21,35 +21,63 @@ def generateFiles(Max = None):
             paths.append(os.path.join(previous, each))
     return paths
 
-def savePaths(outfile, pathList):
-    with open(outfile, "wa") as file:
-        for i in paths:
-            file.write(i+"\n")
-        file.close()
-
 def combineWithFile(filename, pathList):
     newPaths = []
     for i in pathList:
         newPaths.append(os.path.join(i, filename))
     return newPaths
 
+def savePaths(outfile, pathList):
+    with open(outfile, "wa") as file:
+        for i in paths:
+            file.write(i+"\n")
+        file.close()
 
-if __name__ == "__main__":
+def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", default="paths.txt", type=str, help="Write generated paths to file")
-    parser.add_argument("-n", "--number", default=500, type=int, help="Number of maximum paths to be generated")
-    parser.add_argument("-s", "--string", default="", type=str, help="Filename to be included in the paths")
-    
+
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--generate-dirs", default=False, action="store_true" , help="Generate directory paths. If not specified jump to LFI")
     group.add_argument("--generate-files", default=False, action="store_true" , help="Generate file paths. If not specified jump to LFI")
     group.add_argument("--generate-custom", default=False, action="store_true" , help="Generate paths with custom file. If not specified jump to LFI")
 
+    parser.add_argument("-o", "--out-file", default="", type=str, help="Write generated paths to file")
+    parser.add_argument("-n", "--number", default=500, type=int, help="Number of maximum paths to be generated")
+    parser.add_argument("-s", "--string", default="", type=str, help="Filename to be included in the paths")
+    
     args = parser.parse_args()
+    return [parser, args]
+
+def Banner():
+    banner = """
+##############################################################
+ 
+     _     ______ _____      _                  _           
+    | |    |  ___|_   _|    | |                | |          
+    | |    | |_    | | _ __ | |_ _ __ _   _  __| | ___ _ __ 
+    | |    |  _|   | || '_ \| __| '__| | | |/ _` |/ _ \ '__|
+    | |____| |    _| || | | | |_| |  | |_| | (_| |  __/ |   
+    \_____/\_|    \___/_| |_|\__|_|   \__,_|\__,_|\___|_|  
+
+
+############################################### By Diego Bernal
+                """
+    print banner
+
+if __name__ == "__main__":
+
+    parser, args = parse()
+
+    if len(sys.argv) == 1:
+        Banner()
+        parser.print_usage()
+        print
+        sys.exit(0)
+
+    # figure out what to do when no option
 
     if args.__dict__["generate_dirs"] or args.__dict__["generate_files"] or args.__dict__["generate_custom"]:
         
-        outfile  = args.__dict__["file"]
         maxPaths = args.__dict__["number"]
         file_str = args.__dict__["string"]
 
@@ -65,8 +93,6 @@ if __name__ == "__main__":
             paths = generatePaths(maxPaths)
             paths = combineWithFile(file_str, paths)
 
-        
-        savePaths(outfile, paths)
-
-        print paths
+    if args.__dict__["out_file"] != "": 
+        savePaths(args.__dict__["out_file"], paths)
 
