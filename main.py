b25593c9872296f64a23b02d36d59ce4b9543271
DiegoBernal <sc18dba@leeds.ac.uk>
2019-02-27 10:37:37 +0000

    Added argparse support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-import os
+import os, argparse
 
 def generatePaths(Max = None):
     paths = []
@@ -11,9 +11,23 @@ def generatePaths(Max = None):
             paths.append(os.path.join(previous, dir))
     return paths
 
-if __name__ == "__main__":
-    paths = generatePaths(500)
-    with open("paths.txt", "wa") as file:
+def savePaths(outfile, pathList):
+    with open(outfile, "wa") as file:
         for i in paths:
             file.write(i+"\n")
         file.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", default="paths.txt", type=str, help="Write generated paths to file")
+    parser.add_argument("-n", "--number", default=500, type=int, help="Number of maximum paths to be generated")
+    parser.add_argument("-s", "--string", default="", type=str, help="Filename to be searched")
+    args = parser.parse_args()
+
+    outfile  = args.__dict__["file"]
+    maxPaths = args.__dict__["number"]
+    file_str = args.__dict__["string"]
+
+    paths = generatePaths(maxPaths)
+    savePaths(outfile, paths)
+
